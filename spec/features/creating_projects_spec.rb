37eaccac59049ab335462385d0d69e5d580c51cb
John Magee <jemagee@cox.net>
2015-04-03 12:44:10 -0700

    Altered the title display and tested for it

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -7,9 +7,10 @@ RSpec.feature "Creating Projects" do
 		fill_in "Project Name", with: "Sublime Text 3"
 		fill_in "Description", with: "A text editor for everyone"
 		click_button "Create Project"
-
+		project = Project.first
 		expect(page).to have_content("Project has been created.")
-		expect(page).to have_title("Sublime Text 3 - Ticketee - Projects")
+		expect(page).to have_title("Sublime Text 3 - Projects - Ticketee")
+		expect(page.current_url).to eql(project_url(project))
 		
 	end
 end
