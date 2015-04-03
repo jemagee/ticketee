c33d453f740eb0a071a59a2c11dd51a796939db2
John Magee <jemagee@cox.net>
2015-04-03 13:15:27 -0700

    Added and tested presence and length violation to Project Name attribute

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,6 +17,7 @@ class ProjectsController < ApplicationController
 			flash[:notice] = "Project has been created."
 			redirect_to @project
 		else
+			flash.now[:error] = "Project has not been created"
 			render 'new'
 		end
 	end
--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,16 +1,32 @@
 require 'rails_helper'
 
 RSpec.feature "Creating Projects" do
-	scenario "A user can create a new project" do
+
+	before do
 		visit "/"
 		click_link "New Project"
+	end
+
+	scenario "A user can create a new project" do
 		fill_in "Project Name", with: "Sublime Text 3"
 		fill_in "Description", with: "A text editor for everyone"
 		click_button "Create Project"
 		project = Project.first
 		expect(page).to have_content("Project has been created.")
 		expect(page).to have_title("Sublime Text 3 - Projects - Ticketee")
-		expect(page.current_url).to eql(project_url(project))
-		
+		expect(page.current_url).to eql(project_url(project))	
+	end
+
+	scenario "A project without a name is rejected" do
+		click_button "Create Project"
+		expect(page).to have_content("Project has not been created")
+		expect(page).to have_content("Name can't be blank")
+	end
+
+	scenario "A project must be at least 5 characters" do
+		fill_in "Project Name", with: "1234"
+		click_button "Create Project"
+		expect(page).to have_content("Project has not been created")
+		expect(page).to have_content("Name is too short")
 	end
 end
