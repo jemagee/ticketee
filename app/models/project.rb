b5b793c45136532dc0dab99aab73369523a24013
John Magee <jemagee@cox.net>
2015-04-13 12:08:02 -0700

    Implemented ticket viewing and deletion dependence on project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
 
-	has_many :tickets
+	has_many :tickets, dependent: :delete_all
 	
 	validates :name, presence: true, length: {minimum: 5}
 	validates :description, presence: true, length: {minimum: 10}
