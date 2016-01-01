e99756931764b5c6405fc425bf8e387f19c03d67
John Magee <jemagee@cox.net>
2016-01-01 15:14:56 -0800

    Adding add new user functionality within admin name space

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,26 @@
 class Admin::UsersController < Admin::BaseController
   def index
+    @users = User.order(:email)
   end
 
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(get_params)
+    if @user.save
+      flash[:notice] = "User has been created."
+      redirect_to admin_users_path
+    else
+      flash[:alert] = "User has not been created."
+      render 'new'
+    end
+  end
+
+  private
+
+  def get_params
+    params.require(:user).permit(:email, :password)
   end
 end
