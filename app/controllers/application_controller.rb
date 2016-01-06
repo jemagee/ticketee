dad08ec35a07ee949325620dbbf9dc37302c245f
John Magee <jemagee@cox.net>
2016-01-06 12:24:36 -0800

    Creating global authorization error handling default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include Pundit
+
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+
+  private 
+
+  def not_authorized
+  	flash[:alert] = "You aren't allowed to do that!"
+  	redirect_to root_path
+  end
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe ProjectsController, :type => :controller do
 		expect(response).to redirect_to(root_path)
 		expect(flash[:alert]).to eql("The project you were looking for could not be found")
 	end
+
+	it "handles permission errors by redirecting to a safe place" do
+
+		allow(controller).to receive(:current_user)
+
+		project = FactoryGirl.create(:project)
+		get :show, id: project
+
+		expect(response).to redirect_to(root_path)
+		message = "You aren't allowed to do that!"
+		expect(flash[:alert]).to eq message
+	end
 end
